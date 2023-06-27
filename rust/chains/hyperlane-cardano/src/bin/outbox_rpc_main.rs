a55d046312f2345880ddb81181c4901ff8f863bd
Sergei Patrikeev <serejke.best@gmail.com>
2023-06-27 18:39:31 +0100

    feat[cardano]: use RPC library.

--- rust/chains/hyperlane-cardano/src/bin/outbox_rpc_main.rs
+++ rust/chains/hyperlane-cardano/src/bin/outbox_rpc_main.rs
@@ -1,8 +1,8 @@
-use cardano_rpc::apis::default_api::ApiOutboxMessagesGetError;
+use cardano_rpc::apis::default_api::MessagesByBlockRangeError;
 use cardano_rpc::apis::Error;
 use hyperlane_cardano::rpc::outbox_rpc_main;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Error<ApiOutboxMessagesGetError>> {
+async fn main() -> Result<(), Error<MessagesByBlockRangeError>> {
     outbox_rpc_main().await
 }
--- rust/chains/hyperlane-cardano/src/rpc/mod.rs
+++ rust/chains/hyperlane-cardano/src/rpc/mod.rs
@@ -1,28 +1,20 @@
 use cardano_rpc::apis::configuration::Configuration;
-use cardano_rpc::apis::default_api::{api_outbox_messages_get, ApiOutboxMessagesGetError};
+use cardano_rpc::apis::default_api::{messages_by_block_range, MessagesByBlockRangeError};
 use cardano_rpc::apis::Error;
 use serde::{Deserialize, Serialize};
 
 const RPC_URL: &str = "http://localhost:4010";
 
-pub async fn outbox_rpc_main() -> Result<(), Error<ApiOutboxMessagesGetError>> {
+pub async fn outbox_rpc_main() -> Result<(), Error<MessagesByBlockRangeError>> {
     let configuration = configuration();
-    let messages = api_outbox_messages_get(&configuration).await?;
+    let messages = messages_by_block_range(&configuration, 0, 10).await?;
     println!("{:#?}", messages);
 
     Ok(())
 }
 
 fn configuration() -> Configuration {
-    let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert(
-        "Prefer",
-        reqwest::header::HeaderValue::from_static("example=multipleMessages"),
-    );
-    let client = reqwest::Client::builder()
-        .default_headers(headers)
-        .build()
-        .unwrap();
+    let client = reqwest::Client::builder().build().unwrap();
     Configuration {
         base_path: String::from(RPC_URL),
         client,
