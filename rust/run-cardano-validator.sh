c8174eba711806aea4ca277b7d5551f29788438b
Sergei Patrikeev <serejke.best@gmail.com>
2023-07-11 08:13:53 +0100

    feat: script to run validator.

---
+++ rust/run-cardano-validator.sh
@@ -0,0 +1,5 @@
+rm -rf /tmp/cardanoDb/validator/
+mkdir -p /tmp/cardanoDb/validator/
+rm -rf /tmp/test_cardano_checkpoints_0x70997970c51812dc3a010c7d01b50e0d17dc79c8
+source config/cardano/validator.env
+CONFIG_FILES=./config/cardano/cardano.json cargo run --bin validator
