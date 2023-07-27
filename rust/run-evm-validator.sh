05c7eff1047c488d4b1024a76be868cf22c50c42
Sergei Patrikeev <serejke.best@gmail.com>
2023-07-27 20:20:13 +0200

    chore: add script to run EVM validator.

---
+++ rust/run-evm-validator.sh
@@ -0,0 +1,5 @@
+rm -rf /tmp/evmDb/validator/
+mkdir -p /tmp/evmDb/validator/
+rm -rf /tmp/test_evm_checkpoints_0xeb382E56eFF04DA7ad115E494207308bb84d82C3
+source config/cardano/evm.validator.env
+CONFIG_FILES=./config/cardano/cardano.json cargo run --bin validator
