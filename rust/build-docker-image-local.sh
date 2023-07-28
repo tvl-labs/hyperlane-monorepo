38306047b533386f51ca4177f3288d0cd5fafc7c
Sergei Patrikeev <serejke.best@gmail.com>
2023-07-28 12:57:58 +0200

    chore: fix the local Docker image.

--- rust/build-docker-image-local.sh
+++ rust/build-docker-image-local.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-cargo build --release --bin validator --bin relayer --bin scraper
-
 export LOCAL_REGISTRY=localhost:5000
 export TAG=$(date +%Y%m%d%H%M%S)
 export IMAGE=$LOCAL_REGISTRY/hyperlane-cardano-local:$TAG
 
-docker build -f Dockerfile.local -t $IMAGE .
+docker build -t $IMAGE .
 
 docker push $IMAGE
 
