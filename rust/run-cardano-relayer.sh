85d098421bd22770ecac0de72cdde410da08a8ca
Hai | Kreate <hai@kreate.community>
2023-08-01 11:07:28 +0700

    fix: working env for new mailbox

--- rust/run-cardano-relayer.sh
+++ rust/run-cardano-relayer.sh
@@ -1,4 +1,4 @@
-rm -rf /tmp/cardanoDb/relayer/
-mkdir /tmp/cardanoDb/relayer/
+rm -rf /tmp/evmDb/relayer/
+mkdir /tmp/evmDb/relayer/
 source config/cardano/relayer.env
 MERKLE_TREE_HASH_BLAKE2B=true CONFIG_FILES=./config/cardano/cardano.json cargo run --bin relayer
